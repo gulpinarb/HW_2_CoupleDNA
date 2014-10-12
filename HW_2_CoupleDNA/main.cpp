379625f274b4ce573e89ed03433ac4e92121dc03
Pınar <pinar@Pnars-MacBook-Air.local>
2014-10-12 18:50:16 +0300

    Initial Commit

---
+++ HW_2_CoupleDNA/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HW_2_CoupleDNA
+//
+//  Created by Pınar on 12/10/14.
+//  Copyright (c) 2014 Pınar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
